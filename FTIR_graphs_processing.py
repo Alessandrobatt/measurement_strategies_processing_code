9a3ba5646cc2b1535e43c4863497157cc4af804e
Alessandro <alex.battegazzore@gmail.com>
2024-03-21 09:43:21 +0100

    Working version

--- FTIR_graphs_processing.py
+++ FTIR_graphs_processing.py
@@ -4,8 +4,12 @@ import os
 
 data_path = r".\FTIR_source_data"
 
-idx_to_skip = [0, 2, 4, 6, 8]       # index of files to skip
+# idx_to_skip = [0, 2, 4, 6, 8]       # index of files to skip
+# idx_to_plot = [1, 3, 5, 7, 9]       # index of files to plot
+idx_to_plot = [1, 5, 7, 9]
+
 plt.close()
+plt.figure(figsize=(7, 5), tight_layout=True, dpi=250)
 
 for i, file_name in enumerate(os.listdir(data_path)):
 
@@ -19,16 +23,32 @@ for i, file_name in enumerate(os.listdir(data_path)):
         data.append([float(pair[0]), float(pair[1])])
     data = np.array(data)
 
-    if not i in idx_to_skip:
-        plt.plot(data[:,0], data[:,1], label=file_name)
+    if i in idx_to_plot:
+        plt.plot(data[:,0], data[:,1])#, label=)
         print("             [PLOTTED]")
     else:
         print("             [SKIPPED]")
 
 plt.grid()
+# plt.ylim([0, 120])
+# plt.yticks(np.linspace(0, 120, 13))
+plt.ylim([50, 120])
+plt.yticks(np.linspace(50, 120, 8))
+
+
+plt.xlim([600, 4000])
+plt.xticks(np.linspace(600, 4000, 9))
+
 plt.xlabel("Wavenumber $[cm^{-1}]$")
-plt.ylabel("Reflectance $[-]$")
-plt.legend()
+plt.ylabel("Transmittance $[\%]$")
+
+# plt.legend(["Natural Hematite (1) [AEFE01]", "Natural Hematite (2) [AEFE01]"])
+# plt.legend(["Industrial Silicon Oxide (1) [AEQ02]", "Industrial Silicon Oxide (2) [AEQ02]"])
+# plt.legend(["Magnetite (1) [AEFE02]", "Magnetite (2) [AEFE02]"])
+# plt.legend(["Natural Quartz (1) [AEQ01]", "Natural Quartz (2) [AEQ01]"])
+# plt.legend(["Mars Analogue (1) [AEMA01]", "Mars Analogue (2) [AEMA01]"])
+plt.legend(["Hematite", "Mars Analogue", "Natural Quartz", "Industrial Quartz"])
+
 plt.gca().invert_xaxis()
 
 plt.savefig(".\FTIR_all_samples.png")
--- XRD_processing.py
+++ XRD_processing.py
@@ -4,8 +4,17 @@ import numpy as np
 
 data_path = r".\XRD_source_data"
 
+plt.close()
+plt.figure(figsize=(7, 5), tight_layout=True, dpi=250)
+
+# idx_to_plot = [1, 4, 7]
+idx_to_plot = [1,4,7]
+
 for i, file_name in enumerate(os.listdir(data_path)):
     if file_name[-1] == 'c':
+
+        print(f"Processing file with index {i} | series name: {file_name}", end='')
+
         f = open(data_path + "\\" + file_name)
         raw_data = f.readlines()[78:-3]
         # data = [item[:-1].split(',') for item in raw_data]
@@ -17,9 +26,17 @@ for i, file_name in enumerate(os.listdir(data_path)):
 
         pass
 
-        plt.plot(np.linspace(10, 90, len(data)), data, label=file_name)
-        # plt.show()
+        if i in idx_to_plot:
+            plt.plot(np.linspace(10, 90, len(data)), data, linewidth=0.75, label=file_name)
+            print("             [PLOTTED]")
+        else:
+            print("             [SKIPPED]")
 
+
+plt.xlim([10, 90])
+plt.ylim([0, 1800])
+plt.xticks(np.linspace(10, 90, 17))
+plt.yticks(np.linspace(0, 1800, 19))
 plt.xlabel("Angle $2\\theta$ $[\deg]$")
 plt.ylabel("Counts [-]")
 plt.legend()
